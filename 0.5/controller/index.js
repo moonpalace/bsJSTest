e925700138f6563267b9d255bd651a33c8a204f2
SeungHyun PAEK <pekuid@gmail.com>
2014-07-29 15:16:04 +0900

    router 테스트 file, virtual, arguments, method key 추가

--- 0.5/controller/index.js
+++ 0.5/controller/index.js
@@ -1,8 +1,47 @@
+var file, virtual, args, method;
 exports.controller = {
 	index:function( v0, v1 ){
 		bs.log( '/index.js :: index', v0, v1 );
+		file = bs.router('file'),
+		virtual = bs.router('virtual'),
+		args = bs.router('arguments'),
+		method = bs.router('method');
+		bsTest( 'file', function(){
+			return file;
+		}, 'controller/index.js' );
+		bsTest( 'virtual', function(){
+			return virtual;
+		}, '/' );
+		bsTest( 'args1', function(){
+			return args[0];
+		}, v0 );
+		bsTest( 'args2', function(){
+			return args[1];
+		}, v1 );
+		bsTest( 'method', function(){
+			return method;
+		}, 'index' );
 	},
 	test:function( v0, v1 ){
 		bs.log( '/index.js :: test', v0, v1 );
+		file = bs.router('file'),
+		virtual = bs.router('virtual'),
+		args = bs.router('arguments'),
+		method = bs.router('method');
+		bsTest( 'file', function(){
+			return file;
+		}, 'controller/index.js' );
+		bsTest( 'virtual', function(){
+			return virtual;
+		}, '/' );
+		bsTest( 'args1', function(){
+			return args[0];
+		}, v0 );
+		bsTest( 'args2', function(){
+			return args[1];
+		}, v1 );
+		bsTest( 'method', function(){
+			return method;
+		}, 'test' );
 	}
 };
