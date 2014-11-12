58978258bd89be0cfbe7a5711ec6fb3adb285246
oopchoi <oopchoi@gmail.com>
2014-11-12 15:18:16 +0900

    에러 현상

---
+++ 0.6/oopchoi/job/js/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14. 11. 12
+ * Time: 오후 3:10
+ * To change this template use File | Settings | File Templates.
+ */
+bs(function(){
+    console.log("loaded!")
+})();
