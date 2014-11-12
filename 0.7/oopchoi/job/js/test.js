7dd2864602e58ed2bc01843eb989edea69c51b8b
oopchoi <oopchoi@gmail.com>
2014-11-13 08:44:20 +0900

    bs.Dom(this) 에러?

--- 0.7/oopchoi/job/js/test.js
+++ 0.7/oopchoi/job/js/test.js
@@ -5,6 +5,11 @@
  * Time: 오후 3:10
  * To change this template use File | Settings | File Templates.
  */
+var resizeHandler = function(){
+    console.log("resize~~~~!");
+}
+
+bs.Dom(this).S("resize", resizeHandler);
 bs(function(){
-    console.log("loaded!")
+    console.log("loaded!");
 });
